f4bc741f5cd2c950c23401209d792ca88e90204b
June-Park-developer <jungeunp97@naver.com>
2025-11-21 15:10:12 +0900

    Update 완주하지 못한 선수.py
    - Add second solution using collections.Counter

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -5,3 +5,14 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             return participant[i]
     return participant[-1]
+
+
+# collections.Counter
+## 이터러블을 넣으면 각 항목이 몇번 나왔는지를 세어주는 편리한 클래스
+## 심지어 뺄셈, 덧셈도 가능하다!
+
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
