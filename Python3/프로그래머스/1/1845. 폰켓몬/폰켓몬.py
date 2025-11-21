b452d7611dd807b040b72e2be5cc674354c1f48a
June-Park-developer <jungeunp97@naver.com>
2025-11-21 14:37:26 +0900

    Update 폰켓몬.py

--- Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ Python3/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,6 @@
-// 처음 풀이
-// 기존에 담기지 않은 것만 모아서 수를 세되
-// 최대 개수를 넘기면 더 할 필요 없음
+# 처음 풀이
+## 기존에 담기지 않은 것만 모아서 수를 세되
+## 최대 개수를 넘기면 더 할 필요 없음
 
 def solution(nums):
     nums.sort()
@@ -12,7 +12,7 @@ def solution(nums):
             hash.append(num)
     return len(hash)
 
-// set() 를 이용한 풀이
+# set() 를 이용한 풀이
 
 def solution(nums):
     return min(len(nums)//2, len(set(nums)))
