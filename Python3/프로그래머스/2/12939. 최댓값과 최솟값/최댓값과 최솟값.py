f8ed8431b710a3f775aed1946ea40694746d4b78
June-Park-developer <jungeunp97@naver.com>
2025-12-24 09:33:26 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 9.21 MB -BaekjoonHub

--- Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ Python3/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,17 +1,9 @@
 def solution(s):
-    nums = s.split(" ")
-    numbers = list(map(lambda x: int(x), nums))
-    min_num = min(numbers)
-    max_num = max(numbers)
-    return f"{min_num} {max_num}"
-
-# 다른 사람 풀이
-def solution(s):
-    s = list(map(int,s.split()))
-    return str(min(s)) + " " + str(max(s))
+    chars = s.split(" ")
+    numbers = list(map(lambda x:int(x), chars))
+    min_number = min(numbers)
+    max_number = max(numbers)
+    return f"{min_number} {max_number}"
     
-# 가져갈 것
-## map 은 list() 로 한번 감싸줘야 함
-## min(이터러블), max(이터러블)  => 전반적으로 메서드가 어디껀지가 헷갈림;;
-## str() 도 사용 가능
-## f-string 사용법
+    
+    
