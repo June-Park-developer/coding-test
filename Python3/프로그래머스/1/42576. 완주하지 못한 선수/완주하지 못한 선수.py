59dfb12ae0d1c48fa21075c7b30a23315453f510
June-Park-developer <jungeunp97@naver.com>
2025-12-24 09:46:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.56 ms, Memory: 25.4 MB -BaekjoonHub

--- Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ Python3/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,18 +1,7 @@
 def solution(participant, completion):
     participant.sort()
     completion.sort()
-    for i in range(0, len(completion)):
-        if participant[i] != completion[i]:
+    for i in range(0,len(completion)):
+        if completion[i] != participant[i]:
             return participant[i]
     return participant[-1]
-
-
-# collections.Counter
-## 이터러블을 넣으면 각 항목이 몇번 나왔는지를 세어주는 편리한 클래스
-## 심지어 뺄셈, 덧셈도 가능하다!
-
-import collections
-
-def solution(participant, completion):
-    answer = collections.Counter(participant) - collections.Counter(completion)
-    return list(answer.keys())[0]
