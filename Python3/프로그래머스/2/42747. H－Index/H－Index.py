c9de68693a37653ef3825d425b3782667ff5fc4c
June-Park-developer <jungeunp97@naver.com>
2025-12-31 12:34:53 +0900

    [level 2] Title: H-Index, Time: 0.29 ms, Memory: 9.29 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -8,3 +8,5 @@ def solution(citations):
         if count <= cite: candidates.append(count)
     if len(candidates) == 0 : return 0
     return candidates.pop()
+# 코드를 효율적으로 짜보자. 경계를 잘 생각할 것
+# enumerate 를 사용한 코드도 짜보자
