61c8a0218ebc513ec4a3d0650387691bea5e7c33
June-Park-developer <jungeunp97@naver.com>
2026-01-02 08:43:27 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.02 ms, Memory: 9.22 MB -BaekjoonHub

--- Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,6 +1,2 @@
 def solution(strings, n):
-    # n 번째 글자들이 뭔지 일단 확인해야 하고
-    # 그걸 sort 한 다음에..? 
-    # 그게 원래 무슨 단어였는지를 인덱스를 기억해야하는건가? 
-    # 정렬을 구현해야할 것 같은데 전혀 모르겠음 ~~
-    return sorted(strings, key=lambda x: (x[n], x))
+    return sorted(strings, key = lambda x: (x[n], x))
