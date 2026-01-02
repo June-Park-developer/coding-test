9b185941269cc27fd0f52533465b817f3f239d9a
June-Park-developer <jungeunp97@naver.com>
2026-01-02 09:09:35 +0900

    [level 2] Title: H-Index, Time: 0.17 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/2/42747. H－Index/H－Index.py
+++ Python3/프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,12 +1,15 @@
 def solution(citations):
-    citations.sort(reverse = True)
-    count = 0
-    candidates = []
-    h = citations[0]  
-    for cite in citations:
-        count += 1
-        if count <= cite: candidates.append(count)
-    if len(candidates) == 0 : return 0
-    return candidates.pop()
+    h = 0
+    citations.sort(reverse=True)
+    
+    # for i in range(len(citations)):
+    #     if (i+1) > citations[i]:
+    #         return i
+    # return len(citations)
+    
 # 코드를 효율적으로 짜보자. 경계를 잘 생각할 것
 # enumerate 를 사용한 코드도 짜보자
+    for i, cite in enumerate(citations, start=1):
+        if i > cite:
+            return i - 1
+    return len(citations)
