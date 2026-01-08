b0408c480363fb1ed09da50395fce4c53dad2425
June-Park-developer <jungeunp97@naver.com>
2026-01-08 09:46:25 +0900

    [level 3] Title: 네트워크, Time: 1.93 ms, Memory: 9.33 MB -BaekjoonHub

--- Python3/프로그래머스/3/43162. 네트워크/네트워크.py
+++ Python3/프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,39 @@
 def solution(n, computers):
-    answer = 0
-    visited = [False] * n 
-
+    # DFS (재귀)
+#     def dfs(v): 
+#         visited[v] = True
+        
+#         for i in range(n):
+#             if not visited[i] and computers[v][i]:
+#                 dfs(i)
+                
+#     visited = [False] * n
+#     count = 0
+#     for i in range(n):
+#         if not visited[i]:
+#             count +=1
+#             dfs(i)
+        
+        
+#     return count
+    
+    
+    # DFS (스택)
     def dfs(v):
-        visited[v] = True  
-        for neighbor in range(n):
-            
-            if computers[v][neighbor] == 1 and not visited[neighbor]:
-                dfs(neighbor)  
-
+        stack = [v]
+        
+        while stack:
+            current = stack.pop()
+            visited[current] = True
+            for i in range(n):
+                if not visited[i] and computers[current][i]:
+                    stack.append(i)
+    
+    visited = [False] * n
+    count = 0
+    
     for i in range(n):
-        if not visited[i]:  
-            dfs(i)          
-            answer += 1     
-
-    return answer
+        if not visited[i]:
+            count += 1
+            dfs(i)
+    return count
