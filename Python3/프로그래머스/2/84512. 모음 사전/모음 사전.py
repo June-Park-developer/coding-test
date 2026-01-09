8993c54ae395149c26cbceac743e02d91d19c278
June-Park-developer <jungeunp97@naver.com>
2026-01-09 13:52:12 +0900

    [level 2] Title: 모음 사전, Time: 4.14 ms, Memory: 9.37 MB -BaekjoonHub

--- Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,4 +1,5 @@
 def solution(word):
+    # 1. DFS 사용
     dictionary = []
     def dfs(current_word):
         if len(current_word) > 5:
@@ -8,6 +9,6 @@ def solution(word):
         for letter in ['A', 'E', 'I', 'O', 'U']:
             dfs(current_word+letter)
     dfs("")
-    dictionary.sort()
     return dictionary.index(word) + 1
-            
+    
+    # 2. product 라는 라이브러리 사용
