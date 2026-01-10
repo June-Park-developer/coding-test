08d025af50ffd66542f4f393632293194134c6d4
June-Park-developer <jungeunp97@naver.com>
2026-01-10 14:16:08 +0900

    [level 2] Title: 모음 사전, Time: 1.66 ms, Memory: 9.38 MB -BaekjoonHub

--- Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ Python3/프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,14 +1,22 @@
+from itertools import product
 def solution(word):
     # 1. DFS 사용
-    dictionary = []
-    def dfs(current_word):
-        if len(current_word) > 5:
-            return
-        if current_word != "":
-            dictionary.append(current_word)
-        for letter in ['A', 'E', 'I', 'O', 'U']:
-            dfs(current_word+letter)
-    dfs("")
-    return dictionary.index(word) + 1
+#     dictionary = []
+#     def dfs(current):
+#         dictionary.append(current)
+#         if len(current) == 5: return
+#         for l in ['A', 'E', 'I', 'O', 'U']:
+#             dfs(current + l)
+    
+#     dfs("")
+#     return dictionary.index(word)
     
     # 2. product 라는 라이브러리 사용
+    dictionary = []
+    vowels = ['A','E','I','O','U']
+    for i in range(1, 6):
+        for p in product(vowels, repeat=i): # p 는 튜플임
+                dictionary.append("".join(p))
+    dictionary.sort()
+    return dictionary.index(word) +1
+    
